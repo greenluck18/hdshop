41dcd5f3500ce7a307793ff9a8f720622052d81c
Halyna Mazur <greenluck3.14159@gmail.com>
2024-11-16 21:01:17 +0200

    minor changes

---
+++ README.md
@@ -0,0 +1,18 @@
+# Compose honeduke shop
+
+This repository is a composition using Docker of honeduke shop: 
+- [hdshop-backend](https://github.com/greenluck18/hdshop-backend)
+- [hdshop-frontend](https://github.com/greenluck18/hdshop-frontend)
+
+## Compose
+
+1. Change the .env.sample file to .env and fill in the environment variables.
+2. Write the following command to start the containers:
+```bash
+./start.sh
+```
+
+
+# License
+
+This project is licensed under the MIT License - see the LICENSE file for details.
--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
+#read from .env file
+while read -r LINE; do
+  if [[ $LINE == *'='* ]] && [[ $LINE != '#'* ]]; then
+    ENV_VAR="$(echo -e "$LINE" | envsubst)"
+    eval "declare $ENV_VAR"
+  fi
+done <.env
 
 cd tmp
 rm -rf repositories
@@ -5,7 +12,14 @@ mkdir repositories
 cd repositories
 
 git clone git@github.com:greenluck18/hdshop-backend.git
-git clone git@github.com:greenluck18/hdshop-frontend.git
+cd hdshop-backend
+docker build -t hdshop-backend .
+cd ..
+
+# git clone git@github.com:greenluck18/hdshop-frontend.git
+# cd hdshop-frontend
+# docker build -t hdshop-frontend .
+# cd ..
 
 cd ../..
 
